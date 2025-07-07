5203dc30f6276fb12f884e4da6ecb5645d7b2cf7
12799 <anik15-12799@diu.edu.bd>
2025-07-08 04:04:13 +0600

    Almost Done

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -16,12 +16,20 @@ class HomeScreen extends StatelessWidget {
               color: Color(0xFF363f93),
               borderRadius: BorderRadius.only(
                 bottomRight: Radius.circular(50)
-              )
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xFF363f93).withOpacity(0.3),
+                  offset: Offset(0.0, 10.0),
+                  blurRadius: 10,
+                  spreadRadius: 2
+                )
+              ]
             ),
             child: Stack(
               children: [
                 Positioned(
-                  top: 110,
+                  top: 100,
                   left: 0,
                   child: Container(
                     height: 80,
@@ -36,7 +44,7 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
                 Positioned(
-                  top: 130,
+                  top: 120,
                   left: 20,
                   child: Text('Your Books', style: TextStyle(
                     color: Color(0xFF363f93),
@@ -97,41 +105,103 @@ class HomeScreen extends StatelessWidget {
                 ),
                 Positioned(
                   top: 80,
-                  left: 200,
-                  child: Text('Big Miracles', style: TextStyle(
-                    color: Color(0XFF363f93),
-                    fontSize: 20,
-                    fontWeight: FontWeight.w800
-                  ),),
-                ),
-                Positioned(
-                  top: 110,
-                  left: 200,
-                  child: Text('Dylan Ahmed', style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600
-                  ),),
-                ),
-                Positioned(
-                  top: 143,
-                  left: 200,
+                  left: 203,
                   child: Container(
+                    height: 100,
                     width: 150,
-                    height: 1.5,
-                    color: Colors.grey,
-                  )
-                ),
-                Positioned(
-                  top: 150,
-                  left: 200,
-                  child: Text("It's larger than life", style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600
-                  ),),
-                ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Big Miracles', style: TextStyle(
+                            color: Color(0XFF363f93),
+                            fontSize: 20,
+                            fontWeight: FontWeight.w800
+                        ),),
+                        Text('Dylan Ahmed', style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600
+                        ),),
+                        Divider(color: Colors.grey,),
+                        Text("It's larger than life", style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600
+                        ),),
+                      ],
+                    ),
+                  ),
+                )
               ],
+            ),
+          ),
+          SizedBox(height: 20,),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    Stack(
+                      children: [
+                        Container(
+                          height: 160,
+                          width: double.maxFinite,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 19, right: 19),
+                            child: Container(
+                            height: 150,
+                            width: double.maxFinite,
+                            decoration: BoxDecoration(
+                                color: Color(0XFF363f93),
+                                borderRadius: (index % 2 == 0) ?
+                                    BorderRadius.only(
+                                        bottomLeft: Radius.circular(80)
+                                    ) :
+                                    BorderRadius.only(
+                                        topRight: Radius.circular(80)
+                                    ),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Color(0XFF363f93).withOpacity(0.3),
+                                      offset: Offset(-10.0, 10.0),
+                                      blurRadius: 10,
+                                      spreadRadius: 2
+                                  )
+                                ]
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 45,
+                          left: 45,
+                          child: Container(
+                            height: 100,
+                            width: 210,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('THE BEST', style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w400
+                                ),),
+                                Text('Your life starts here', style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold
+                                ),),
+                              ],
+                            ),
+                          ),
+                        )
+                     ]
+                    ),
+                    SizedBox(height: 35,),
+                  ],
+                );
+              },
             ),
           )
         ],
